8ed2b7a65722ce13e17f0cc946929c36db339458
Jon Sime <jonsime@gmail.com>
2012-09-21 20:20:15 -0400

    plugin for converting characters in input string to latin-1 supplemental (semi-randomly) to provide a facsimile of the swedish chef's talking style

---
+++ lib/RoboBot/Plugin/Bork.pm
@@ -0,0 +1,58 @@
+package RoboBot::Plugin::Bork;
+
+use strict;
+use warnings;
+
+use utf8;
+
+sub commands { qw( bork ) }
+sub usage { "<message>" }
+
+my %repl = (
+    A   => ['A',"\N{U+00c4}","\N{U+00c5}"],
+    O   => ['O',"\N{U+00d6}","\N{U+00d8}"],
+    U   => ['U',"\N{U+00dc}"],
+    AE  => ["\N{U+00c6}"],
+    Ae  => ["\N{U+00c6}"],
+    SS  => ["\N{U+00df}"],
+    Ss  => ["\N{U+00df}"],
+    TH  => ['TH',"\N{U+00de}"],
+    Th  => ['Th',"\N{U+00de}"],
+
+    a   => ['a',"\N{U+00e4}","\N{U+00e5}"],
+    o   => ['o',"\N{U+00f6}","\N{U+00f8}"],
+    oo  => ["\N{U+00fc}\N{U+00fc}\N{U+00fc}\N{U+00fc}"],
+    u   => ['u',"\N{U+00fc}"],
+    ae  => ["\N{U+00e6}"],
+    ss  => ['ss',"\N{U+00df}"],
+    th  => ['th',"\N{U+00f0}"],
+);
+
+sub handle_message {
+    my ($class, $bot, $sender, $channel, $command, $original, $timestamp, $message) = @_;
+
+    return unless $message;
+
+    foreach my $ch (sort { length($b) <=> length($a) } keys %repl) {
+#        my $new_ch = rand() <= 0.5 ? $repl{$ch}[int(rand(scalar(@{$repl{$ch}})))] : $repl{$ch}->[0];
+
+        $message =~
+            s<
+                ($ch)+
+            ><
+                rand() <= 0.8
+                    ? $repl{$ch}[int(rand(scalar(@{$repl{$ch}})))]
+                    : $repl{$ch}->[0]
+            >gex;
+    }
+
+    my @words = split(/\s+/, $message);
+
+    foreach my $word (@words) {
+        $word = 'bork bork bork' if rand() <= 0.1;
+    }
+
+    return join(' ', @words);
+}
+
+1;
