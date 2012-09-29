3fd4973a18367ce27841461b289019de5117d1c6
Jon Sime <jonsime@gmail.com>
2012-09-29 16:03:59 -0400

    basic Karma tracking plugin, when people do nick++/nick-- in channel

---
+++ lib/RoboBot/Plugin/Karma.pm
@@ -0,0 +1,106 @@
+package RoboBot::Plugin::Karma;
+
+use strict;
+use warnings;
+
+sub commands { qw( * karma ) }
+sub usage { "<nick>" }
+
+sub handle_message {
+    my ($class, $bot, $sender, $channel, $command, $original, $timestamp, $message) = @_;
+
+    print "Karma plugin got message [[$message]]\n";
+
+    if ($command && $command eq 'karma') {
+        return display_karma($bot, $message);
+    } elsif ($message =~ m{(\w+)[:,]?\s*(\-\-|\+\+)}o) {
+        print "Karma operation for $1: $2\n";
+
+        return add_karma($bot, $sender, $1) if $2 eq '++';
+        return remove_karma($bot, $sender, $1) if $2 eq '--';
+    }
+
+    return -1;
+}
+
+sub display_karma {
+    my ($bot, $message) = @_;
+
+    return unless $message =~ m{\b(\w+)\b}o;
+    my $nick = $1;
+
+    my $res = $bot->{'dbh'}->do(q{
+        select n.nick, sum(k.karma) as karma
+        from karma_karma k
+            join nicks n on (n.id = k.nick_id)
+        where n.nick = ?
+        group by n.nick
+    }, $nick);
+
+    return unless $res && $res->next;
+
+    return sprintf('%s has %.2f karma', $res->{'nick'}, $res->{'karma'});
+}
+
+sub add_karma {
+    my ($bot, $sender, $nick) = @_;
+
+    my $sender_id = nick_id($bot, $sender);
+    return unless $sender_id;
+
+    my $nick_id = nick_id($bot, $nick);
+    return unless $nick_id;
+
+    print "Adding karma to $nick_id from $sender_id\n";
+
+    my $res = $bot->{'dbh'}->do(q{
+        insert into karma_karma (nick_id, from_nick_id, karma) values (?,?,?)
+    }, $nick_id, $sender_id, 1);
+
+    return -1;
+}
+
+sub remove_karma {
+    my ($bot, $sender, $nick) = @_;
+
+    my $sender_id = nick_id($bot, $sender);
+    return unless $sender_id;
+
+    my $nick_id = nick_id($bot, $nick);
+    return unless $nick_id;
+
+    my $res = $bot->{'dbh'}->do(q{
+        insert into karma_karma (nick_id, from_nick_id, karma) values (?,?,?)
+    }, $nick_id, $sender_id, -1);
+
+    return -1;
+}
+
+sub nick_id {
+    my ($bot, $sender) = @_;
+
+    $sender =~ s{\_+$}{}og;
+
+    return $bot->{'db'}->{'nicks'}->{$sender}
+        if $bot->{'db'}->{'nicks'} && $bot->{'db'}->{'nicks'}->{$sender};
+
+    my $res = $bot->{'dbh'}->do(q{ select id from nicks where nick = ? }, $sender);
+
+    $bot->{'db'}->{'nicks'} = {} unless $bot->{'db'}->{'nicks'};
+
+    if ($res && $res->next) {
+        $bot->{'db'}->{'nicks'}->{$sender} = $res->{'id'};
+
+        return $res->{'id'};
+    } else {
+        $res = $bot->{'dbh'}->do(q{ insert into nicks (nick) values (?) returning id }, $sender);
+
+        return unless $res && $res->next;
+
+        $bot->{'db'}->{'nicks'}->{$sender} = $res->{'id'};
+
+        return $res->{'id'};
+    }
+}
+
+1;
