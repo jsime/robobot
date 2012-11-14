6395232987e65eef75d5b620457f9b2597862c3e
Jon Sime <jonsime@gmail.com>
2012-11-13 20:54:41 -0500

    Pick plugin for having robobot choose randomly from a list of things

---
+++ lib/RoboBot/Plugin/Pick.pm
@@ -0,0 +1,25 @@
+package RoboBot::Plugin::Pick;
+
+use strict;
+use warnings;
+
+sub commands { qw( pick ) }
+sub usage { "choice1, choice2, ..., choiceN" }
+
+sub handle_message {
+    my ($class, $bot, $sender, $channel, $command, $original, $timestamp, $message) = @_;
+
+    my @choices = grep { $_ =~ m{\S+} } split(/\s*,\s*/, $message);
+
+    return unless scalar(@choices) > 0;
+
+    my $choice = $choices[int(rand(scalar(@choices)))];
+
+    return sprintf('%s: I cannot make up my mind.', $sender) unless $choice =~ m{\S+};
+
+    $choice =~ s{(^\s+|\s+$)}{}ogs;
+
+    return sprintf('%s: %s', $sender, $choice);
+}
+
+1;
