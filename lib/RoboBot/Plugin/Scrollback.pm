47fcb397260bd668606c59972c5e4870b87e8ffc
Jon Sime <jonsime@gmail.com>
2013-01-15 21:45:39 -0500

    scrollback plugin now redirects its output directly to the sender, so channels won't be spammed

--- lib/RoboBot/Plugin/Scrollback.pm
+++ lib/RoboBot/Plugin/Scrollback.pm
@@ -9,10 +9,15 @@ sub usage { "[minutes]" }
 sub handle_message {
     my ($class, $bot, $sender, $channel, $command, $original, $timestamp, $message) = @_;
 
-    my $minutes = 60;
+    my $minutes = 240;
     if ($message =~ m{\b(\d+)\b}o) {
         $minutes = $1;
     }
+
+    # We set the limit on the query to 1 higher than the allowed message
+    # limit we pass back to the main routine, so that if there were more
+    # channel messages within the specified time limit, the recipient will
+    # see an "output truncated" notice.
 
     my $res = $bot->{'dbh'}->do(q{
         select to_char(l.posted_at, 'HH24:MI') as posted_at, n.nick, l.message
@@ -21,19 +26,22 @@ sub handle_message {
             join channels c on (c.id = l.channel_id)
         where c.name = ? and l.posted_at >= now() - (interval '1 minute' * ?)
         order by l.posted_at desc
-        limit 10
+        limit 21
     }, $channel, $minutes);
 
     return unless $res;
 
-    my @r;
+    my @r = ({ to => $sender, msg_limit => 20 });
 
     while ($res->next) {
-        push(@r, sprintf('%s <%s> %s', $res->{'posted_at'}, $res->{'nick'}, $res->{'message'}));
+        push(@r, sprintf('%s [%s] <%s> %s', $res->{'posted_at'}, $channel,
+            $res->{'nick'}, $res->{'message'}));
     }
 
-    return @r if scalar(@r) > 0;
-    return 'Nothing found.';
+    # >1 (not 0) because there will always be the options hashref in @r
+    push(@r, 'Nothing found.') unless scalar(@r) > 1;
+
+    return @r;
 }
 
 1;
