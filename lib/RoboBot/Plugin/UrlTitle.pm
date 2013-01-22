44cd26c7a27f8f68fa676443e983ce52733f07be
Jon Sime <jonsime@gmail.com>
2013-01-21 19:49:07 -0500

    updated URL titler plugin to handle multiple URLs per message, instead of only catching the first one it sees per line

--- lib/RoboBot/Plugin/UrlTitle.pm
+++ lib/RoboBot/Plugin/UrlTitle.pm
@@ -12,35 +12,37 @@ sub commands { qw( * ) }
 sub handle_message {
     my ($class, $bot, $sender, $channel, $command, $original, $timestamp, $message) = @_;
 
-    if ($message =~ m{\b(https?://\S+)\b}oi) {
-        my $url = $1;
+    my @urls = ($message =~ m{\b(https?://\S+)\b}oig);
 
+    if (scalar(@urls) > 0) {
         my $ua = LWP::UserAgent->new();
         $ua->timeout(3); # short timeout because we don't want to hold up other plugins/commands
 
-        my $r = $ua->get($url);
+        my @output = ();
 
-        if ($r->is_success) {
-            my $parser = HTML::HeadParser->new();
-            $parser->parse($r->decoded_content());
+        foreach my $url (@urls) {
+            my $r = $ua->get($url);
 
-            my $title = $parser->header('Title');
+            if ($r->is_success) {
+                my $parser = HTML::HeadParser->new();
+                $parser->parse($r->decoded_content());
 
-            my $final_url = $r->base;
+                my $title = $parser->header('Title');
 
-            my @output = ();
+                my $final_url = $r->base;
 
-            push(@output, sprintf('Title: %s', (length($title) > 120 ? substr($title, 0, 110) . '...' : $title)))
-                if $title =~ m{\w+};
+                push(@output, sprintf('Title: %s', (length($title) > 120 ? substr($title, 0, 110) . '...' : $title)))
+                    if $title =~ m{\w+};
 
-            # seems a little odd, but direct comparisons weren't working reliably, maybe because of
-            # character set mismatches? anyway -- only show redirect if the levenshtein between the
-            # two URLs is greater than X% of the lenght of the input URL.
-            push(@output, sprintf('Redirected to: %s', $final_url))
-                if distance($final_url, $url) >= length($url) * 0.1;
-
-            return scalar(@output) > 0 ? @output : -1;
+                # seems a little odd, but direct comparisons weren't working reliably, maybe because of
+                # character set mismatches? anyway -- only show redirect if the levenshtein between the
+                # two URLs is greater than X% of the lenght of the input URL.
+                push(@output, sprintf('Redirected to: %s', $final_url))
+                    if distance($final_url, $url) >= length($url) * 0.1;
+            }
         }
+
+        return scalar(@output) > 0 ? @output : -1;
     }
 
     return (-1);
