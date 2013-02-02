28ad9e396ad93bc99190637b067e83aa0e53eaf3
Jon Sime <jonsime@gmail.com>
2013-02-02 12:06:05 -0500

    ping plugin output now includes response time

--- lib/RoboBot/Plugin/Ping.pm
+++ lib/RoboBot/Plugin/Ping.pm
@@ -4,6 +4,7 @@ use strict;
 use warnings;
 
 use Net::Ping;
+use Time::HiRes qw( gettimeofday tv_interval );
 
 sub commands { qw( ping ) }
 sub usage { "[hostname|ip]" }
@@ -13,11 +14,17 @@ sub handle_message {
 
     return "pong!" unless $message;
 
+    $message =~ s{(^\s+|\s+$)}{}ogs;
+
     my $p = Net::Ping->new('udp');
+
+    my $t0 = [gettimeofday];
     my $r = $p->ping($message);
+    my $t1 = [gettimeofday];
+
     $p->close();
 
-    return sprintf('%s is responding.', $message) if $r;
+    return sprintf('%s responded in %.2fms.', $message, tv_interval($t0, $t1)*1000) if $r;
     return sprintf('%s is NOT responding.', $message);
 }
 
