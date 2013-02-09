473776094735bbed8e87a7af04ee79999b002844
Jon Sime <jonsime@gmail.com>
2013-02-08 21:03:57 -0500

    extended last command from Logger plugin to allow multiple nicks to be specified, as well as offset,limit instead of just offset, for retrieving multiple log lines at once

--- lib/RoboBot/Plugin/Logger.pm
+++ lib/RoboBot/Plugin/Logger.pm
@@ -4,19 +4,22 @@ use strict;
 use warnings;
 
 sub commands { qw( * last ) }
-sub usage { "[nick] [num]" }
+sub usage { "[nick[,nick,...]] [offset[,count]]" }
 
 sub handle_message {
     my ($class, $bot, $sender, $channel, $command, $original, $timestamp, $message) = @_;
 
     if ($command eq 'last') {
+        # collapse whitespace around commas, so it's easier for people to list multiple nicks
+        $message =~ s{\s*,\s*}{,}ogs;
+
         my @opt = split(/\s+/, $message);
 
         my ($nick, $num);
 
-        if (scalar(@opt) == 2 && $opt[1] =~ m{^\d+$}o) {
+        if (scalar(@opt) == 2 && $opt[1] =~ m{^\s*\d+(,\d+)?\s*$}o) {
             ($nick, $num) = @opt;
-        } elsif (scalar(@opt) == 1 && $opt[0] =~ m{^\d+$}o) {
+        } elsif (scalar(@opt) == 1 && $opt[0] =~ m{^\s*\d+(,\d+)?\s*$}o) {
             $num = $opt[0];
         } elsif (scalar(@opt) == 1) {
             $nick = $opt[0];
@@ -34,26 +37,73 @@ sub handle_message {
 sub last_message {
     my ($bot, $channel, $nick, $num) = @_;
 
-    my $nick_where = '';
-    my @binds = ();
+    my @nicks;
 
-    $nick_where = qq{ and nick_id = (select nx.id from nicks nx where nx.nick = ?) } if $nick;
-    push(@binds, $nick) if $nick;
-
-    my $res = $bot->db->do(qq{
-        select n.nick, ll.message
-        from logger_log ll
-            join nicks n on (n.id = ll.nick_id)
-        where ll.channel_id = ? $nick_where
-        order by ll.posted_at desc
-        limit 1 offset ?
-    }, $bot->{'db'}->{'channels'}->{$channel}, @binds, $num > 0 ? $num - 1 : 0);
-
-    if ($res && $res->next) {
-        return sprintf('<%s> %s', $res->{'nick'}, $res->{'message'});
-    } else {
-        return 'Nothing found.';
+    if (defined $nick && $nick =~ m{\w+}o) {
+        @nicks = map { $_ =~ s{(^\s+|\s+$)}{}ogs; lc($_) } split(/,/, $nick);
     }
+
+    my $limit = 1;
+    my $offset = 0;
+
+    if (defined $num && $num =~ m{^\s*(\d+)\s*$}o) {
+        $offset = $1 - 1;
+    } elsif (defined $num && $num =~ m{^\s*(\d+),(\d+)\s*$}o) {
+        $offset = $1 - 1;
+        $limit = $2;
+    }
+
+    # for now, force a limit of 5 messages (for sanity)
+    $limit = 5 if $limit > 5;
+
+    # because the offset is moving backwards, we need to make sure the limit doesn't
+    # exceed it (as the limit then moves the end point back toward the most recent entry)
+    $limit = ($offset + 1) if $limit >= $offset;
+
+    # then we move the offset to mark the end point (closest to most recent) since we'll
+    # be grabbing these in reverse order initially
+    $offset = ($offset + 1) - $limit;
+
+    my $res;
+
+    if (scalar(@nicks) > 0) {
+        $res = $bot->db->do(q{
+            select d.*
+            from ( select n.nick, l.message, l.posted_at
+                   from nicks n
+                       join logger_log l on (l.nick_id = n.id)
+                       join channels c on (c.id = l.channel_id)
+                   where lower(n.nick) in ??? and lower(c.name) = lower(?)
+                   order by l.posted_at desc
+                   limit ? offset ?
+                 ) d
+            order by d.posted_at asc
+        }, \@nicks, $channel, $limit, $offset);
+    } else {
+        $res = $bot->db->do(q{
+            select d.*
+            from ( select n.nick, l.message, l.posted_at
+                   from logger_log l
+                       join channels c on (c.id = l.channel_id)
+                       join nicks n on (n.id = l.nick_id)
+                   where lower(c.name) = lower(?)
+                   order by l.posted_at desc
+                   limit ? offset ?
+                 ) d
+            order by d.posted_at asc
+        }, $channel, $limit, $offset);
+    }
+
+    my @r;
+
+    if ($res) {
+        while ($res->next) {
+            push(@r, sprintf('<%s> %s', $res->{'nick'}, $res->{'message'}));
+        }
+    }
+
+    return 'Nothing found.' unless scalar(@r) > 0;
+    return join(' ', @r);
 }
 
 sub log_message {
