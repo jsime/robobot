416e5549973bf9c9367825c26ec5911081486c1d
Jon Sime <jonsime@gmail.com>
2013-03-03 12:01:42 -0500

    support optional leading s on regexes

--- lib/RoboBot/Plugin/Sub.pm
+++ lib/RoboBot/Plugin/Sub.pm
@@ -4,12 +4,12 @@ use strict;
 use warnings;
 
 sub commands { qw( sub ) }
-sub usage { "/<pattern>/<replacement>/ [source]" }
+sub usage { "s/<pattern>/<replacement>/ [source]" }
 
 sub handle_message {
     my ($class, $bot, $sender, $channel, $command, $original, $timestamp, $message) = @_;
 
-    return unless $message =~ m{^/(.*)(?<!\x{5c})/(.*)/\s+(.*)}o;
+    return unless $message =~ m{^s?/(.*)(?<!\x{5c})/(.*)/\s+(.*)}o;
 
     my $search = $1;
     my $replace = $2;
