f707b9731c52b336b9a8fc96702d0ff69e8e9393
Jon Sime <jonsime@gmail.com>
2013-03-07 18:49:49 -0500

    added brace style support for sub command

--- lib/RoboBot/Plugin/Sub.pm
+++ lib/RoboBot/Plugin/Sub.pm
@@ -1,5 +1,8 @@
 package RoboBot::Plugin::Sub;
 
+use utf8;
+use 5.014;
+
 use strict;
 use warnings;
 
@@ -9,15 +12,29 @@ sub usage { "s/<pattern>/<replacement>/ [source]" }
 sub handle_message {
     my ($class, $bot, $sender, $channel, $command, $original, $timestamp, $message) = @_;
 
-    return unless $message =~ m{^s?/(.*)(?<!\x{5c})/(.*)/\s+(.*)}o;
+    my ($source);
 
-    my $search = $1;
-    my $replace = $2;
-    my $source = $3;
+    my $slash = "/\N{U+2044}\N{U+2215}\N{U+ff0f}";
 
-    eval {
-        $source =~ s{$search}{$replace}ig;
-    };
+    if ($message =~ m|^\s*s?[$slash](.*)(?<!\x{5c})[$slash](.*)(?<!\x{5c})[$slash]\s+(.*)|ou) {
+        my $search = $1;
+        my $replace = $2;
+        $source = $3;
+
+        eval {
+            $source =~ s{$search}{$replace}igu;
+        };
+    } elsif ($message =~ m|^\s*s{(.*)(?<!\x{5c})}\s*{(.*)(?<!\x{5c})}\s+(.*)|ou) {
+        my $search = $1;
+        my $replace = $2;
+        $source = $3;
+
+        eval {
+            $source =~ s{$search}{$replace}uigx;
+        };
+    } else {
+        return;
+    }
 
     return $source unless $@;
     return;
