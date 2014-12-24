1d7bb22b40ff7b60d947621460e5b7f4232f0e45
Jon Sime <jonsime@gmail.com>
2014-12-23 23:49:36 -0500

    new base Plugin class to handle common functionality based on individual plugin metadata -- each plugin is expected to use has +attr overrides to fill in effectively everything aside from specific function implementations

---
+++ lib/RoboBot/Plugin.pm
@@ -0,0 +1,103 @@
+package RoboBot::Plugin;
+
+use strict;
+use warnings FATAL => 'all';
+
+use Moose;
+use MooseX::SetOnce;
+use namespace::autoclean;
+
+has 'name' => (
+    is  => 'ro',
+    isa => 'Str',
+);
+
+has 'description' => (
+    is        => 'ro',
+    isa       => 'Str',
+    predicate => 'has_description',
+);
+
+has 'commands' => (
+    is      => 'ro',
+    isa     => 'HashRef',
+    default => sub { return {} },
+);
+
+has 'before_hook' => (
+    is        => 'ro',
+    isa       => 'Str',
+    predicate => 'has_before_hook',
+);
+
+has 'after_hook' => (
+    is        => 'ro',
+    isa       => 'Str',
+    predicate => 'has_after_hook',
+);
+
+has 'bot' => (
+    is     => 'rw',
+    isa    => 'RoboBot',
+    traits => [qw( SetOnce )],
+);
+
+sub process {
+    my ($self, $message, $command, @args) = @_;
+
+    return $message->response->raise("Invalid command processor executed.")
+        unless exists $self->commands->{$command};
+
+    my $method = $self->commands->{$command}{'method'};
+
+    # By default, we pre-process all arguments, but some plugins can opt out
+    # of this to handle things like conditional evaluations or loops
+    unless (exists $self->commands->{$command}{'preprocess_args'} && $self->commands->{$command}{'preprocess_args'} == 0) {
+        my @processed_args;
+
+        # TODO this duplicates code from RoboBot.pm in process_list (which this even
+        # calls) -- they should be get refactored to use a common set of list
+        # processing code instead (especially for clean handling of variables)
+        foreach my $arg (@args) {
+            if (ref($arg) eq 'ARRAY') {
+                push(@processed_args, $self->bot->process_list($message, $arg));
+            } else {
+                if (exists $message->vars->{$arg}) {
+                    if (ref($message->vars->{$arg}) eq 'ARRAY') {
+                        push(@processed_args, $self->bot->process_list($message, $arg));
+                    } else {
+                        push(@processed_args, $message->vars->{$arg});
+                    }
+                } else {
+                    push(@processed_args, $arg);
+                }
+            }
+        }
+
+        @args = @processed_args;
+    }
+
+    return $self->$method($message, $command, @args);
+}
+
+sub hook_before {
+    my ($self, $message) = @_;
+
+    return $message unless $self->has_before_hook;
+
+    my $hook = $self->before;
+    return $self->$hook($message);
+}
+
+sub hook_after {
+    my ($self, $message) = @_;
+
+    return $message unless $self->has_after_hook;
+
+    my $hook = $self->after;
+    return $self->$hook($message);
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
