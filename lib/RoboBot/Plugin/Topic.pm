d53abca20846454e289da3d66e6cba7581588211
Jon Sime <jonsime@gmail.com>
2015-01-10 00:12:17 -0500

    channel topic setter/getter function

---
+++ lib/RoboBot/Plugin/Topic.pm
@@ -0,0 +1,44 @@
+package RoboBot::Plugin::IRC;
+
+use strict;
+use warnings FATAL => 'all';
+
+use Moose;
+use MooseX::SetOnce;
+use namespace::autoclean;
+
+extends 'RoboBot::Plugin';
+
+has '+name' => (
+    default => 'IRC',
+);
+
+has '+description' => (
+    default => 'Provides a variety of functions for performing IRC-related actions like channel topics, oper promotion/demotion, etc.',
+);
+
+has '+commands' => (
+    default => sub {{
+        'topic' => { method      => 'channel_topic',
+                     description => 'When a new topic string is given, changes the channel\'s topic to that string. In all uses, returns the channel topic as a string.',
+                     usage       => '["<new topic>"]' },
+    }},
+);
+
+sub channel_topic {
+    my ($self, $message, $command, $new_topic) = @_;
+
+    return unless $message->has_channel;
+
+    my $channel_name = '#' . $message->channel->channel;
+
+    if (defined $new_topic) {
+        $self->bot->irc->yield( topic => '#' . $channel_name, $new_topic );
+    }
+
+    return $self->bot->irc->yield( topic => $channel_name );
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
