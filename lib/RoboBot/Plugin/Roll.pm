168184ec262133a769655b96851cb417b3c92029
Jon Sime <jonsime@gmail.com>
2015-05-23 13:33:48 -0400

    clean up some of the input handling for die sizes and roll counts

--- lib/RoboBot/Plugin/Roll.pm
+++ lib/RoboBot/Plugin/Roll.pm
@@ -23,7 +23,7 @@ has '+commands' => (
     default => sub {{
         'roll' => { method          => 'roll',
                     preprocess_args => 1,
-                    usage           => '<die size> <die count>',
+                    usage           => '<die size> [<die count>]',
                     example         => '2 10',
                     result          => '17' },
     }},
@@ -34,20 +34,39 @@ sub roll {
 
     my $nf = Number::Format->new();
 
-    if (@args && @args == 2 && $args[0] =~ m{^\d+$}o && $args[1] =~ m{^\d+$}o) {
-        my $result = 0;
-        for (1..$args[1]) {
-            $result += int(rand($args[0])) + 1;
-        }
-        $message->response->push(sprintf('You rolled a %s-sided die %s times for a total of %s.',
-            $nf->format_number($args[0]),
-            $nf->format_number($args[1]),
-            $nf->format_number($result)));
-        return $result;
+    unless (@args && @args > 0) {
+        return $message->response->raise('Invalid die size and roll count arguments.');
     }
 
-    # invalid arguments, short-circuit respose with standard usage message
-    return $self->usage($message, 'roll');
+    unless ($args[0] =~ m{^\d+$}o) {
+        return $message->response->raise('Die size must always be expressed as an integer.');
+    }
+
+    my $size = $args[0];
+    my $rolls = 1;
+
+    if (defined $args[1]) {
+        unless ($args[1] =~ m{^\d+$}o) {
+            return $message->response->raise('Number of rolls must be expressed as an integer. Omitting the roll count will default to 1.');
+        }
+
+        $rolls = $args[1];
+    }
+
+    if ($size > 2**16 || $rolls > 2**16) {
+        return $message->response->raise('My arms cannot handle that much dice rolling. Please try smaller numbers');
+    }
+
+    my $result = 0;
+    for (1..$rolls) {
+        $result += int(rand($size)) + 1;
+    }
+    $message->response->push(sprintf('You rolled a %s-sided die %s time%s for a total of %s.',
+        $nf->format_number($size),
+        $nf->format_number($rolls),
+        $rolls != 1 ? 's' : '',
+        $nf->format_number($result)));
+    return $result;
 }
 
 __PACKAGE__->meta->make_immutable;
