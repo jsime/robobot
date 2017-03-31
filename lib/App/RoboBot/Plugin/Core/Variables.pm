a859a56cffa3b506531cf64a4e47237459bbce5e
Jon Sime <jonsime@gmail.com>
2017-03-31 05:52:05 +0000

    gut and replace core.variables functions to provide more reasonable set of functions and allow binding expressions

--- lib/App/RoboBot/Plugin/Core/Variables.pm
+++ lib/App/RoboBot/Plugin/Core/Variables.pm
@@ -10,19 +10,22 @@ use MooseX::SetOnce;
 use Data::Dumper;
 use Scalar::Util qw( blessed );
 
+use App::RoboBot::Parser;
+
 extends 'App::RoboBot::Plugin';
 
 =head1 core.variables
 
 Provides functions to create and manage variables.
 
-There are two types of variables in App::RoboBot: message scope and global. Message
-scope variables persist only as long as it takes to process the current message
-and then they are destroyed. Global variables, however, persist indefinitely
-and are available across all channels on a single network. They may be re-used,
-changed, and undefined from anywhere on the network they were initialized, by
-anyone with permission to call the ``(set-global)`` or ``(unset-global)``
-functions.
+There are two basic classes of variables in App::Robobot: global and lexically
+scoped variables. The latter are handled by the ``let`` form and are not
+covered here.
+
+Global variables, those created with ``def`` are persistent across messages and
+are accessible from all channels on the current network. They may be reused by
+multiple messages, have their value updated repeatedly, and may be undefined by
+any message on the same network. They are not visible across networks.
 
 =cut
 
@@ -34,230 +37,374 @@ has '+description' => (
     default => 'Provides functions to create and manage variables.',
 );
 
-=head2 defined
+=head2 def
 
 =head3 Description
 
+Defines a new global variable with the given value (or values, which will store
+the list of values in the global). Note that if a quoted expression is
+provided, it is captured and only evaluated when the variable is used. What
+sets this apart from defining a macro is that global variable expressions
+receive no arguments.
+
+If the name of a global variable is provided, but no value or expression
+follows, the definition of the variable is returned.
+
+Repeated use of ``def`` with the same variable name will clobber prior values.
+
 =head3 Usage
+
+<name> [<value or quoted expression>]
 
 =head3 Examples
 
-=head2 setvar
+    :emphasize-lines: 3,6
+
+    (def two 2)
+    (val two)
+    2
+
+    (def two)
+    (def two 2)
+
+=head2 defined?
 
 =head3 Description
 
+Returns true if the name provided is that of a currently defined global. False
+in all other cases.
+
 =head3 Usage
+
+<name>
 
 =head3 Examples
 
-=head2 unsetvar
+    :emphasize-lines: 3,6
+
+    (def two 2)
+    (defined? two)
+    1
+
+    (defined? this-is-not-a-variable)
+    0
+
+=head2 decr
 
 =head3 Description
 
+Given the name of a global variable which exists and stores a scalar numeric
+value, decrements that stored value by 1 and returns the newly stored value.
+The use of a global variable name which currently does not exist initializes a
+global variable with the value of zero. If the variable exists, but contains
+anything other than a scalar numeric, no change is made to the stored value and
+a zero is returned.
+
 =head3 Usage
 
+<name>
+
 =head3 Examples
+
+    :emphasize-lines: 3,6,9
+
+    (def my-counter 50)
+    (decr my-counter)
+    49
+
+    (val my-counter)
+    49
+
+    (decr non-existent-counter)
+    0
 
 =head2 incr
 
 =head3 Description
 
+Given the name of a global variable which exists and stores a scalar numeric
+value, increments that stored value by 1 and returns the newly stored value.
+The use of a global variable name which currently does not exist initializes a
+global variable with the value of zero. If the variable exists, but contains
+anything other than a scalar numeric, no change is made to the stored value and
+a zero is returned.
+
 =head3 Usage
+
+<name>
 
 =head3 Examples
 
-=head2 set-global
+    :emphasize-lines: 3,6,9
+
+    (def my-counter 50)
+    (incr my-counter)
+    51
+
+    (val my-counter)
+    51
+
+    (incr non-existent-counter)
+    0
+
+=head2 undef
 
 =head3 Description
 
+If given the name of an extant global variable, undefines it while returning
+the current value. Returns nil if the variable name does not exist.
+
 =head3 Usage
+
+<name>
 
 =head3 Examples
 
-=head2 unset-global
+    :emphasize-lines: 3,6
+
+    (def two 2)
+    (undef two)
+    2
+
+    (undef two)
+    nil
+
+=head2 val
 
 =head3 Description
 
-=head3 Usage
-
-=head3 Examples
-
-=head2 var
-
-=head3 Description
+Returns the current value of the named global variable. Returns nil if the
+variable does not exist. If the variable name references an expression, that
+expression is evaluated and its result is returned.
 
 =head3 Usage
 
+<name>
+
 =head3 Examples
+
+    :emphasize-lines: 3,7,10
+
+    (def two 2)
+    (val two)
+    2
+
+    (def two-by-mathing '(+ 1 1))
+    (def two-by-mathing)
+    (+ 1 1)
+
+    (val two-by-mathing)
+    2
 
 =cut
 
 has '+commands' => (
     default => sub {{
-        'defined'      => { method => 'is_defined',    preprocess_args => 0 },
-        'setvar'       => { method => 'set_var',       preprocess_args => 0 },
-        'unsetvar'     => { method => 'unset_var',     preprocess_args => 0 },
-        'incr'         => { method => 'increment_var', preprocess_args => 0 },
-        'set-global'   => { method => 'set_global' },
-        'unset-global' => { method => 'unset_global' },
-        'var'          => { method => 'get_global' },
+        'def'      => { method => 'var_define', preprocess_args => 0 },
+        'defined?' => { method => 'var_defined' },
+        'decr'     => { method => 'var_decrement' },
+        'incr'     => { method => 'var_increment' },
+        'undef'    => { method => 'var_undefine' },
+        'val'      => { method => 'var_value' },
     }},
 );
 
-sub set_global {
-    my ($self, $message, $command, $rpl, $var_name, @values) = @_;
+sub var_define {
+    my ($self, $message, $command, $rpl, $name, @values) = @_;
 
-    unless (defined $var_name && $var_name =~ m{\w+}) {
-        $message->response->raise('Must provide a variable name and at least one value.');
+    if (defined $name && blessed($name) && $name->can('evaluate')) {
+        $name = $name->evaluate($message, $rpl);
+    } else {
+        $message->response->raise('Must provide a name for the global variable.');
         return;
     }
 
-    unless (@values) {
-        return $self->unset_global($message, $command, $var_name);
+    if (@values < 1) {
+        # No new values provided, so show the definition without any evaluation
+        my $res = $self->bot->config->db->do(q{
+            select *
+            from global_vars
+            where network_id = ?
+                and lower(var_name) = lower(?)
+        }, $message->network->id, $name);
+
+        unless ($res && $res->next) {
+            $message->response->raise('No such global variable %s.', $name);
+            return;
+        }
+
+        return @{$res->{'var_values'}};
     }
+
+    my @flattened = map { $_->flatten } @values;
 
     my $res = $self->bot->config->db->do(q{
         update global_vars
-        set ???
+        set var_values = ?,
+            created_by = ?,
+            updated_at = now()
         where network_id = ?
             and lower(var_name) = lower(?)
-    }, {
-        var_values => \@values,
-        updated_at => 'now',
-    }, $message->network->id, $var_name);
+        returning *
+    }, \@flattened, $message->sender->id, $message->network->id, $name);
 
-    if ($res && $res->count > 0) {
-        $message->response->push(sprintf('Global variable %s has been updated.', $var_name));
+    if ($res && $res->next) {
+        $message->response->push(sprintf('Global variable %s updated.', $name));
         return;
-    }
-
-    $res = $self->bot->config->db->do(q{
-        insert into global_vars ??? returning *
-    }, {
-        network_id => $message->network->id,
-        var_name   => $var_name,
-        var_values => \@values,
-        created_by => $message->sender->id,
-    });
-
-    if ($res && $res->next && $res->{'id'} =~ m{\d+}) {
-        $message->response->push(sprintf('Global variable %s has been set.', $var_name));
-        return;
-    }
-
-    $message->response->raise('Could not set global variable %s. Please check your input and try again.', $var_name);
-    return;
-}
-
-sub unset_global {
-    my ($self, $message, $command, $rpl, $var_name) = @_;
-
-    unless (defined $var_name && $var_name =~ m{\w+}) {
-        $message->response->raise('Must provide a variable name to unset it.');
-        return;
-    }
-
-    my $res = $self->bot->config->db->do(q{
-        delete from global_vars where network_id = ? and lower(var_name) = lower(?)
-    }, $message->network->id, $var_name);
-
-    $message->response->push(sprintf('Global variable %s has been unset.', $var_name))
-        if $res && $res->count > 0;
-    return;
-}
-
-sub get_global {
-    my ($self, $message, $command, $rpl, $var_name, $default) = @_;
-
-    unless (defined $var_name && $var_name =~ m{\w+}) {
-        $message->response->raise('Must provide a variable name to retrieve a value.');
-        return;
-    }
-
-    my $res = $self->bot->config->db->do(q{
-        select var_values
-        from global_vars
-        where network_id = ? and lower(var_name) = lower(?)
-    }, $message->network->id, $var_name);
-
-    if ($res && $res->next && defined $res->{'var_values'}) {
-        return @{$res->{'var_values'}};
-    } elsif (defined $default) {
-        return $default;
     } else {
+        $res = $self->bot->config->db->do(q{
+            insert into global_vars ??? returning *
+        }, {
+            network_id => $message->network->id,
+            var_name   => $name,
+            var_values => \@flattened,
+            created_by => $message->sender->id,
+        });
+
+        if ($res && $res->next) {
+            $message->response->push(sprintf('Global variable %s created.', $name));
+            return;
+        }
+    }
+
+    $message->response->raise('Error while working with %s global variable definition.', $name);
+    return;
+}
+
+sub var_defined {
+    my ($self, $message, $command, $rpl, $name) = @_;
+
+    unless (defined $name && $name =~ m{w\+}) {
+        $message->response->raise('Must provide a variable name to check for defined-ness.');
         return;
     }
+
+    my $res = $self->bot->config->db->do(q{
+        select id
+        from global_vars
+        where network_id = ?
+            and lower(var_name) = lower(?)
+    }, $message->network->id, $name);
+
+    return 1 if $res && $res->next;
+    return 0;
 }
 
-sub is_defined {
-    my ($self, $message, $command, $rpl, @var_list) = @_;
+sub var_decrement {
+    my ($self, $message, $command, $rpl, $name) = @_;
 
-    return 0 unless @var_list > 0;
+    return $self->var_adjust($message, $name, -1);
+}
 
-    my $defined = 1;
+sub var_increment {
+    my ($self, $message, $command, $rpl, $name) = @_;
 
-    foreach my $var (@var_list) {
-        if (blessed($var) && $var->can('type')) {
-            $defined = 0 unless $var->has_value;
+    return $self->var_adjust($message, $name, 1);
+}
+
+sub var_adjust {
+    my ($self, $message, $name, $amount) = @_;
+
+    unless (defined $name && $name =~ m{w\+}) {
+        $message->response->raise('Must provide a variable name to increment or decrement.');
+        return;
+    }
+
+    my $res = $self->bot->config->db->do(q{
+        select id, var_values
+        from global_vars
+        where network_id = ?
+            and lower(var_name) = lower(?)
+    }, $message->network->id, $name);
+
+    if ($res && $res->next) {
+        if (@{$res->{'var_values'}} == 1 && $res->{'var_values'}[0] =~ m{^\d+$}o) {
+            $res = $self->bot->config->db->do(q{
+                update global_vars
+                set var_value = ?,
+                    updated_at = now()
+                where id = ?
+            }, [$res->{'var_values'}[0] + $amount], $res->{'id'});
+
+            return $res->{'var_values'}[0] + $amount if $res;
+
+            $message->response->raise('Could not adjust %s. Please try again.', $name);
+            return $res->{'var_values'}[0];
         } else {
-            unless (defined $var) {
-                $defined = 0;
-                last;
-            }
+            $message->response->raise('Global variable %s is not a scalar numeric.', $name);
+            return;
+        }
+    } else {
+        $res = $self->bot->config->db->do(q{
+            insert into global_vars ???
+        }, {
+            network_id  => $message->network->id,
+            var_name    => $name,
+            var_values  => [0],
+            created_by  => $message->sender->id
+        });
 
-            if (ref($var)) {
-                $var = $var->evaluate($message, $rpl);
-            }
+        return 0 if $res;
 
-            unless (defined $var) {
-                $defined = 0;
-                last;
-            }
+        $message->response->raise('Could not create new global variable %s.', $name);
+        return;
+    }
+
+    return;
+}
+
+sub var_undefine {
+    my ($self, $message, $command, $rpl, $name) = @_;
+
+    unless (defined $name && $name =~ m{\w+}o) {
+        $message->response->raise('Must provide name of global variable to undefine.');
+        return;
+    }
+
+    my $res = $self->bot->config->db->do(q{
+        delete from global_vars
+        where network_id = ?
+            and lower(var_name) = lower(?)
+    }, $message->network->id, $name);
+
+    return if $res;
+
+    $message->response->raise('Could not undefine global variable %s.', $name);
+    return;
+}
+
+sub var_value {
+    my ($self, $message, $command, $rpl, $name) = @_;
+
+    unless (defined $name && $name =~ m{\w+}o) {
+        $message->response->raise('Must provide a global variable name who value you are retrieving.');
+        return;
+    }
+
+    my $res = $self->bot->config->db->do(q{
+        select *
+        from global_vars
+        where network_id = ?
+            and lower(var_name) = lower(?)
+    }, $message->network->id, $name);
+
+    unless ($res && $res->next) {
+        $message->response->raise('No such global variable %s.', $name);
+        return;
+    }
+
+    my @ret;
+    my $parser = App::RoboBot::Parser->new( bot => $self->bot );
+
+    foreach my $v (@{$res->{'var_values'}}) {
+        my $expr = $parser->parse($v);
+
+        if (defined $expr && blessed($expr) && $expr->can('evaluate')) {
+            push(@ret, $expr->evaluate($message, $rpl));
         }
     }
 
-    return $defined;
-}
-
-sub set_var {
-    my ($self, $message, $command, $rpl, @args) = @_;
-
-    if (@args && @args == 2 && $args[0] =~ m{^[\$\@\*\:\+0-9a-zA-Z_-]+$}) {
-        return $message->vars->{$args[0]} = $message->process_list($args[1]);
-    }
-}
-
-sub unset_var {
-    my ($self, $message, $command, $rpl, @args) = @_;
-
-    if (@args && @args == 1) {
-        if (exists $message->vars->{$args[0]}) {
-            return delete $message->vars->{$args[0]};
-        } else {
-            return $message->response->raise('No such variable.');
-        }
-    }
-}
-
-sub increment_var {
-    my ($self, $message, $command, $rpl, $var_name, $amount) = @_;
-
-    $amount = 1 unless defined $amount;
-
-    unless (defined $var_name && exists $self->message->vars->{$var_name}) {
-        return $message->response->raise('Variable name unspecified or invalid.');
-    }
-
-    unless ($amount =~ m{\d+}o && m{^-?\d*\.\d*$}o) {
-        return $message->response->raise('Increment amount "%s" does not appear to be a valid number.', $amount);
-    }
-
-    unless ($self->message->vars->{$var_name} =~ m{^-?\d*\.\d*$}o) {
-        return $message->response->raise('Variable "%s" is not numeric. Cannot increment.', $var_name);
-    }
-
-    $self->message->vars->{$var_name} += $amount;
-    return $self->message->vars->{$var_name};
+    return @ret;
 }
 
 __PACKAGE__->meta->make_immutable;
